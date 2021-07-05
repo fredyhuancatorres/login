22eb7bf0a8a586d617f293fd971184eb515d604c
Fredy Huanca Torres <fhuancat@unsa.edu.pe>
2021-07-05 11:13:56 -0500

    add dbsqlite

---
+++ gestionpermisos.py
@@ -0,0 +1,10 @@
+
+from tkinter import ttk
+from tkinter import *
+import tkinter.messagebox  as tk
+import sqlite3
+
+conn = sqlite3.connect('basededatos.db')
+cur = conn.cursor()
+
+conn.execute("CREATE TABLE balance(empleado_i text, licencia_enfermedad int, licencia_maternindad int, licencia_emergencia int)")
